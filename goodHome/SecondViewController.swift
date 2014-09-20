76c67a06a67fa88bb1aa234e6df0aca81e6fa62e
jumbo ken <subsubmailer@gmail.com>
2014-09-20 17:23:09 +0900

    Initial Commit

---
+++ goodHome/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  goodHome
+//
+//  Created by 羽田 健太郎 on 2014/09/20.
+//  Copyright (c) 2014年 me.jumbeeee.ken. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
